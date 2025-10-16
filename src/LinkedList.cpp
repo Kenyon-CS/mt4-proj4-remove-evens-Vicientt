77de460a5067ae1eed38bc97985b589a7d653547
Vicientt <nguyen15@kenyon.edu>
2025-10-16 14:24:12 -0400

    Part 4

--- src/LinkedList.cpp
+++ src/LinkedList.cpp
@@ -9,6 +9,29 @@ LinkedList::~LinkedList() {
 void LinkedList::push_front(int v) { head_ = new Node(v, head_); }
 void LinkedList::remove_evens() {
     // TODO: implement
+    while (head_ != nullptr && head_->value % 2 == 0) {
+        Node* node_to_delete = head_;
+        head_ = head_->next;
+        delete node_to_delete;
+    }
+
+    if (head_ == nullptr) {
+        return;
+    }
+
+    Node* prev = head_;
+    Node* current = head_->next;
+
+    while (current != nullptr) {
+        if (current->value % 2 == 0) {
+            prev->next = current->next; 
+            delete current;             
+            current = prev->next;       
+        } else {
+            prev = current;
+            current = current->next;
+        }
+    }
 }
 std::string LinkedList::to_string() const {
     std::ostringstream oss;
